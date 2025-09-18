147ee54239bd9bc441b82071b2a685d63588f04a
tumit <tumit@live.com>
2025-09-18 23:23:26 +0700

    feat(auth): add findByUsrename for login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,11 +3,19 @@ import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { LoginDto } from './dto/login.dto';
 import { TokensDto } from './dto/tokens.dto';
+import { UsersService } from '@app/users/users.service';
 
 @Injectable()
 export class AuthService {
-  login(loginDto: LoginDto): TokensDto {
-    const accessToken = 'VALID_TOKEN'
+
+  constructor(private usersService: UsersService) {}
+
+  async login(loginDto: LoginDto): Promise<TokensDto> {
+
+    // find by username
+    const user = await this.usersService.findByUsername(loginDto.username)
+
+    const accessToken = 'VALID_TOKEN-' + user.username
     return { accessToken };
   }
 }
