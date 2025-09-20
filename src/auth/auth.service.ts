4d9fd09a7dc31a93be8254d27ee5bef6d7f3b43c
tumit <tumit@live.com>
2025-09-20 09:27:27 +0700

    feat(auth): add compare password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { LoginDto } from './dto/login.dto';
 import { TokensDto } from './dto/tokens.dto';
 import { UsersService } from '@app/users/users.service';
-
+import bcrypt from 'bcrypt'
 @Injectable()
 export class AuthService {
 
@@ -14,6 +14,12 @@ export class AuthService {
 
     // find by username
     const user = await this.usersService.findByUsername(loginDto.username)
+
+    // compare hashed-password
+    const matched = await bcrypt.compare(loginDto.password, user.password)
+    if (!matched) {
+      throw new UnauthorizedException(`wrong password: username=${loginDto.username}`)
+    }
 
     const accessToken = 'VALID_TOKEN-' + user.username
     return { accessToken };
